1f332186a1cce0bee585c4d5bc1a798bd4a7fb3f
Arpan Sarkar <arpan98sarkar@gmail.com>
2021-02-23 00:16:50 +0530

    End of Section 4 - 'Added Extension methods'

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,7 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -37,28 +38,14 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<DataContext>(op => 
-            {
-                op.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+           services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
             services.AddCors();
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(op => 
-            {
-             op.TokenValidationParameters = new TokenValidationParameters 
-             {
-                 ValidateIssuerSigningKey = true,
-                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                 ValidateIssuer = false,
-                 ValidateAudience = false
-             };
-            });
+           services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
